0110fe8d2b177be9f1c765cb84b24d2c282b9300
Mikkel RINGAUD <mikkel@milescode.dev>
2023-07-07 01:10:14 +0200

    styles: lesson page

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, presetUno } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
 
 import transformerVariantGroup from '@unocss/transformer-variant-group'
-import presetRemToPx from '@unocss/preset-rem-to-px'
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   presets: [
@@ -11,6 +12,7 @@ export default defineConfig({
 
   transformers: [
     transformerVariantGroup(),
+    transformerDirectives()
   ],
 
   theme: {
