550178ce545187a7bab71540ae512fb759f4ac9e
Mikkel RINGAUD <mikkel@milescode.dev>
2023-07-16 16:56:14 +0200

    fix: page layouts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,23 @@
 import { z, defineCollection } from "astro:content";
 
+const lessonsCollectionSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  tags: z.array(z.string()),
+  sources: z.array(z.string())
+});
+
 export const collections = {
   "html": defineCollection({ 
     type: "content",
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      tags: z.array(z.string()),
-      sources: z.array(z.string())
-    })
+    schema: lessonsCollectionSchema
+  }),
+  "javascript": defineCollection({ 
+    type: "content",
+    schema: lessonsCollectionSchema
+  }),
+  "css": defineCollection({ 
+    type: "content",
+    schema: lessonsCollectionSchema
   })
 };
